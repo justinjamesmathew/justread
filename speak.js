d032a77c57c7f555a9ba77be448fa430fc0492ba
Justin Mathew <justinmathew@Justins-Air.fritz.box>
2022-07-06 20:47:55 +0200

    now you can click on the chrome extension button to start page reading if the chrome extension button is pinned

--- speak.js
+++ speak.js
@@ -2,8 +2,8 @@
 console.log("welcome to speak");
 let para = document.getElementsByTagName('p');
 console.log(para.length);
-for(let i = 0; i < para.length; i ++){
-    let utterance = new SpeechSynthesisUtterance(para[i].innerHTML);
-    
-    setTimeout(speechSynthesis.speak(utterance), 5000);
-}
+if (para.length > 1)
+    for(let i = 0; i < 2; i ++){
+        let utterance = new SpeechSynthesisUtterance(para[i].innerHTML);
+        speechSynthesis.speak(utterance);
+        }
